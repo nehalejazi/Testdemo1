b51a66718f061fdf3ac19c7735ca618b06c28cff
nehalejazi <mnejazi@gmail.com>
2022-03-12 17:36:56 +0000

    Test Commit 1 with basic code

---
+++ src/com/goalwit/Test1.java
@@ -0,0 +1,10 @@
+package com.goalwit;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello'");
+
+	}
+
+}
---
+++ src/com/goalwit/Test2.java
@@ -0,0 +1,9 @@
+package com.goalwit;
+
+public class Test2 {
+	
+	public static void main(String[] args) {
+		System.out.println("Print test 2");
+	}
+
+}
